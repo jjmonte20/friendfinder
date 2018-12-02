15123e5539bce68d3cc30bbfbb599c7183c8f0bb
jjmonte20 <jjmonte20@gmail.com>
2018-12-02 15:20:22 -0600

    added requirement for file system

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,2 @@
+// this application is meant to get a route for /survey
+// also meant to have a catch-all route that leads to home.html
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 // want express, less typing to get sites in an online server
 const express = require("express");
+// const for path
+const path = require("path");
 // const for app
 const app = express();
 // port I will be using, only need one server
 const PORT = 8080;
+// require file system
+const fs = require("fs");
 
 // when port is accessed, should go to the home page
 app.get("/", function(req, res){
