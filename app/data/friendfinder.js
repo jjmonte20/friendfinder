ab6e4b8d4ba6935c976e6e4e4a4a5316870cc543
jjmonte20 <jjmonte20@gmail.com>
2018-12-04 16:37:40 -0600

    linked images to online photos

--- app/data/friendfinder.js
+++ app/data/friendfinder.js
@@ -10,7 +10,7 @@ var friends = [
     // should have a name
     name: "Big Bird",
     // should have a photo
-    photo: "../public/images/big_birg.jpg",
+    photo: "https://secure.i.telegraph.co.uk/multimedia/archive/02869/big_bird1_2869483b.jpg",
     // should also have scores for each question, 10 questions in total
     score: [
         3,
@@ -27,7 +27,7 @@ var friends = [
 },{
     routeName: "oscarTheGrouch",
     name: "Oscar the Grouch",
-    photo: "../public/images/oscar.jpg",
+    photo: "https://ids.si.edu/ids/deliveryService?id=NMAH-JN2018-01024-000004&max=300",
     score: [
        1,
        1,
@@ -43,7 +43,7 @@ var friends = [
 },{
     routeName: "elmo",
     name: "Elmo",
-    photo: "../public/images/elmo.jpg",
+    photo: "https://upload.wikimedia.org/wikipedia/en/7/74/Elmo_from_Sesame_Street.gif",
     score: [
        5,
        5,
@@ -59,7 +59,7 @@ var friends = [
 },{
     routeName: "gritty",
     name: "Gritty",
-    photo: "../public/images/gritty.jpg",
+    photo: "https://cnet4.cbsistatic.com/img/l5Ot1KCGMZg0qnjBxt2_s3RIERw=/970x0/2018/09/25/5b1a7cdf-f052-41f4-9ebb-3357f855f36c/mygod.jpg",
     score: [
        3,
        3,
